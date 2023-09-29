a4f61495464389ff2bfeb035034fc9423055d039
Sourabh Jana <sourabhjana22550@gmail.com>
2023-09-29 15:38:53 +0530

    pwa is implemented

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa"; // Import the PWA plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      // PWA options go here
+      manifest: {
+        // Manifest options go here
+        name: "Bhumi Mapon",
+        short_name: "BhumiMaopn",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/favicon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      // You can add more PWA options here as needed
+    }),
+  ],
+});
